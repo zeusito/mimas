f711821b38ab163725e381cb3c7a30a4f81c9779
Julio <jmonroyq@gmail.com>
2026-07-20 11:19:06 -0700

    removing better-auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,10 @@
-import type { User, Session } from 'better-auth';
+
 
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
-		interface Locals { user?: User; session?: Session }
+		interface Locals { }
 
 		// interface Error {}
 		// interface PageData {}
@@ -13,4 +13,4 @@ declare global {
 	}
 }
 
-export {};
+export { };
